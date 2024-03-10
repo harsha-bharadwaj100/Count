cbe7837e676959bb31ae0c49dbed0c9f3eb897b6
Harsha Bharadwaj <88173012+harsha-bharadwaj100@users.noreply.github.com>
2024-03-10 07:27:30 +0530

    progress

--- app/index.js
+++ app/index.js
@@ -1,10 +1,30 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { React, useState } from "react";
+import { Text, View, SafeAreaView, ScrollView } from "react-native";
+import { Stack, useRouter, Navigator } from "expo-router";
+import { COLORS, SIZES, icons, images } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
-export default function App() {
+export default function Home() {
+  const router = useRouter();
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Hello World!</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{ headerStyle: { backgroundColor: COLORS.lightWhite } }}
+      ></Stack.Screen>
+      <Text>Home</Text>
+    </SafeAreaView>
   );
 }
+
+// export default function App() {
+//   return (
+//     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+//       <Text>Hello World!</Text>
+//     </View>
+//   );
+// }
